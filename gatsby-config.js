a0d1fc54157b5dfd65b03b7eeee719c791234d6f
Felipe Rodrigues <felipefernandorodrigues@gmail.com>
2020-06-07 15:56:19 -0300

    colocando query pegando do strap io

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `ao3`,
+    description: `ao3 description`,
+    author: `Felipe Rodrigues`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +11,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -27,6 +34,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000,
+        contentTypes: [`homes`]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,60 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import './styles.css'
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql `
+    {
+      jobs: allStrapiHomes {
+        nodes {
+          subtitulo
+          tipo
+          titulo
+          id
+          ativo
+          image {
+            childImageSharp {
+              fluid {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          overflow: "auto",
+          height: "1000px"
+        }}>
+        {
+          data.jobs.nodes.map(node => (
+            <BackgroundImage
+              key={node.id}
+              fluid={node.image.childImageSharp.fluid}
+              style={{
+                width: "50%",
+                color: "white",
+              }}
+              >
+              <h3>{node.titulo}</h3>
+              <h1>{node.subtitulo}</h1>
+            </BackgroundImage>
+          ))
+        }
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
